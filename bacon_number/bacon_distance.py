b1d6b4cd2c1530563032a1fc9439f08acc904aad
Or Levi <orlevi78@gmail.com>
2026-03-16 11:14:34 +0200

    Fix technical bugs

--- bacon_number/bacon_distance.py
+++ bacon_number/bacon_distance.py
@@ -1,8 +1,10 @@
 import sqlite3
 
+from .consts import DB_NAME
+
 
 def find_distance_from_bacon(name: str) -> int:
-    conn = sqlite3.connect("/Users/orlevi/Documents/bis-projects/Bacon-Number/movies_and_actors.db")
+    conn = sqlite3.connect(DB_NAME)
     cursor = conn.cursor()
 
     cursor.execute(f"SELECT bacon_number FROM transitions WHERE nconst = '{name}'")
--- bacon_number/consts.py
+++ bacon_number/consts.py
@@ -1,4 +1,6 @@
 NUMBER_OF_ROWS_TO_READ = 10000000
 PATH_TO_DATA = "/Users/orlevi/Documents/bis-projects/Bacon-Number/tests/simple_demo2.tsv"
 BACON_NAME = "bacon"
-DB_NAME = "movies_and_actors.db"
+DB_NAME = "/Users/orlevi/Documents/bis-projects/Bacon-Number/bacon_number/movies_and_actors.db"
+
+# TODO: Change to relative path later.
