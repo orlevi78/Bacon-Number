c7aa3a1df3bc142660b0a60d65da9e72f354df0b
Or Levi <orlevi78@gmail.com>
2026-03-16 10:34:03 +0200

    Better algo. Create the DB already with bacon_number for each person

--- generate_db.py
+++ generate_db.py
@@ -1,7 +1,12 @@
+from typing import Dict, List
+
 import pandas as pd
 import sqlite3
 
-NUMBER_OF_ROWS = 2000000
+NUMBER_OF_ROWS = 10000000
+
+visited: Dict[str, bool] = {}
+
 
 conn = sqlite3.connect("movies_and_actors.db")
 cursor = conn.cursor()
@@ -35,22 +40,65 @@ def create_movies_table():
 
 
 def create_transitions_tables():
-    cursor.execute("CREATE TABLE IF NOT EXISTS transitions (ID INT PRIMARY KEY, tconst TEXT, nconst TEXT)")
+    cursor.execute("CREATE TABLE IF NOT EXISTS transitions (nconst TEXT PRIMARY KEY, bacon_number INT)")
     conn.commit()
     title_basics = pd.read_csv(
-        "imdb_files/title.principals.tsv", sep="\t", usecols=["tconst", "nconst"], nrows=NUMBER_OF_ROWS
+        "/Users/orlevi/Documents/bis-projects/Bacon-Number/tests/simple_demo2.tsv",
+        sep="\t",
+        usecols=["tconst", "nconst"],
+        # nrows=NUMBER_OF_ROWS,
     )
     tconsts_list = list(title_basics["tconst"])
     nconst_list = list(title_basics["nconst"])
-    values = list(zip([i for i in range(NUMBER_OF_ROWS)], tconsts_list, nconst_list))
-    cursor.executemany("INSERT INTO transitions (ID, tconst, nconst) VALUES (?, ?, ?)", values)
+    # print(tconsts_list)
+    # values = list(zip([i for i in range(NUMBER_OF_ROWS)], tconsts_list, nconst_list))
+    # cursor.executemany("INSERT INTO transitions (ID, tconst, nconst) VALUES (?, ?, ?)", values)
+    # conn.commit()
+
+    bacon_number = 0
+    target_list = [("bacon", bacon_number)]
+    colleagues = ["bacon"]
+    visited["bacon"] = True
+    new_colleagues: List[str] = []
+    for colleague in colleagues:
+        new_colleagues += find_colleagues(colleague, nconst_list, tconsts_list)
+    # print(new_colleagues)
+    # new_colleagues = [find_colleagues(colleague) for colleague in colleagues]
+    while len(new_colleagues) > 0:
+        bacon_number += 1
+        target_list += ((colleague, bacon_number) for colleague in new_colleagues)
+        colleagues = new_colleagues
+        new_colleagues = []
+        for colleague in colleagues:
+            new_colleagues += find_colleagues(colleague, nconst_list, tconsts_list)
+
+    cursor.executemany("INSERT INTO transitions (nconst, bacon_number) VALUES (?, ?)", target_list)
     conn.commit()
 
 
+def find_colleagues(name: str, nconst_list, tconsts_list) -> List[str]:
+    print(f"name: {name}")
+    indexes = [index for index, value in enumerate(nconst_list) if value == name]
+    # print(f"indexes: {indexes}")
+    movies = [tconsts_list[index] for index in indexes]
+    movies_indexes = [index for index, value in enumerate(tconsts_list) if value in movies]
+    colleagues = [nconst_list[index] for index in movies_indexes if visited.get(nconst_list[index]) is None]
+    for colleague in colleagues:
+        visited[colleague] = True
+    return colleagues
+
+
+def create_correct_table():
+    pass
+
+
 def main():
-    create_people_table()
-    create_movies_table()
+    # create_people_table()
+    # create_movies_table()
     create_transitions_tables()
+
+    cursor.execute("SELECT * FROM transitions")
+    print(cursor.fetchall())
 
 
 if __name__ == "__main__":
