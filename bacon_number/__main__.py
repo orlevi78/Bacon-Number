bdd1c9cacf94464212e873f3168cce8c5b0bb76f
Or Levi <orlevi78@gmail.com>
2026-03-16 17:36:04 +0200

    Fix CR comments

--- bacon_number/__main__.py
+++ bacon_number/__main__.py
@@ -12,7 +12,7 @@ def main():
     if cursor.fetchone() is None:
         print("Creating transitions DB.")
         create_table()
-    cursor.execute("SELECT name FROM sqlite_master WHERE name='people'")
+    cursor.execute("SELECT name FROM sqlite_master WHERE name='people' AND type='table'")
     if cursor.fetchone() is None:
         print("Creating people DB")
         create_people_table()
