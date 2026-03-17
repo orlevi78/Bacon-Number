5458c2576a7bbca835a23077773bf66b4fe38bac
Or Levi <orlevi78@gmail.com>
2026-03-17 12:02:15 +0200

    Working website. Still ugly.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,27 @@
+import { useState } from "react"
+
 function App() {
 
-  const get_bacon_number = (name: string) => {
-    console.log("here")
+  const [personName, setPersonName] = useState("");
+
+  const get_bacon_number = () => {
+    fetch(`http://localhost:5000/${personName}`).then(response => response.json()).then(data => setPersonName(decode_response_data(data))).catch(error => console.log('Error:', error));
+  }
+
+  const decode_response_data = (data: string) => {
+    if (data == "-1") {
+      return `Error! Person ${personName} is unknown.`
+    }
+    else if (data == "-2") {
+      return `Error! ID ${personName} is unknown.`
+    }
+    return data
   }
 
   return (
     <>
-      <input placeholder="Enter actor's name!" />
-      <button onClick={() => get_bacon_number("here")}>Find Bacon number</button>
+      <input placeholder="Enter actor's name!" value={personName} onInput={e => setPersonName(e.target.value)} />
+      <button onClick={get_bacon_number}>Find Bacon number</button>
     </>
 
   )
--- server/main.py
+++ server/main.py
@@ -1,14 +1,15 @@
 from bacon_number import bacon_distance
 
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask("BaconNumber")
+CORS(app)  # Allows other browser to send requests. Not sure why it's necessary, havn't read much about it.
 
 
 @app.get("/<name>")
 def bacon_number(name: str):
-    print(name.upper())
-    return str(bacon_distance.find_distance_from_bacon("Gal Gadot"))
+    return str(bacon_distance.find_distance_from_bacon(name))
 
 
 app.run(debug=True)
