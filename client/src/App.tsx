5da028e66ce5d7a8953eb7b62be7109a166963b5
Or Levi <orlevi78@gmail.com>
2026-03-17 13:40:02 +0200

    Make the website a bit prettier

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,30 +1,38 @@
-import { useState } from "react"
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-
   const [personName, setPersonName] = useState("");
+  const [result, setResult] = useState("");
 
   const get_bacon_number = () => {
-    fetch(`http://localhost:5000/${personName}`).then(response => response.json()).then(data => setPersonName(decode_response_data(data))).catch(error => console.log('Error:', error));
-  }
+    fetch(`http://localhost:5000/${personName}`)
+      .then((response) => response.json())
+      .then((data) => setResult(decode_response_data(data)))
+      .then(() => setPersonName(""))
+      .catch((error) => console.log("Error:", error));
+  };
 
   const decode_response_data = (data: string) => {
     if (data == "-1") {
-      return `Error! Person ${personName} is unknown.`
+      return `Error! Person: '${personName}' is unknown.`;
+    } else if (data == "-2") {
+      return `Error! ID: '${personName}' is unknown.`;
     }
-    else if (data == "-2") {
-      return `Error! ID ${personName} is unknown.`
-    }
-    return data
-  }
+    return `Bacon number of ${personName} is: ${data}`;
+  };
 
   return (
-    <>
-      <input placeholder="Enter actor's name!" value={personName} onInput={e => setPersonName(e.target.value)} />
+    <div className="user-input-output">
+      <input
+        placeholder="Enter actor's name or ID as written in IMDB!"
+        value={personName}
+        onInput={(e) => setPersonName(e.target.value)}
+      />
       <button onClick={get_bacon_number}>Find Bacon number</button>
-    </>
-
-  )
+      <label>{result}</label>
+    </div>
+  );
 }
 
-export default App
+export default App;
