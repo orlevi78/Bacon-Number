481eea0e9b6cab868c11ff502481f55c515e8ec9
Or Levi <orlevi78@gmail.com>
2026-03-18 14:49:42 +0200

    Remove unnecessary files

--- server/bacon_number/__main__.py
+++ server/bacon_number/__main__.py
@@ -1,8 +1,7 @@
 import sqlite3
 
-from .bacon_distance import find_distance_from_bacon
 from .generate_db import create_table, create_people_table
-from .consts import DB_NAME, PERSON_NOT_FOUND_ERROR, ID_NOT_FOUND_ERROR
+from .consts import DB_NAME
 
 
 def main():
@@ -16,15 +15,6 @@ def main():
     if cursor.fetchone() is None:
         print("Creating people DB")
         create_people_table()
-
-    # name = input("Please enter the name of the person you wish to search for, or their id as written in IMDB\n")
-    # bacon_number = find_distance_from_bacon(name)
-    # if bacon_number == PERSON_NOT_FOUND_ERROR:
-    #     print(f"Error! Person: '{name}' is unknown.")
-    # elif bacon_number == ID_NOT_FOUND_ERROR:
-    #     print(f"Error! ID: {name} is unkown.")
-    # else:
-    #     print(f"Bacon number of '{name}' is: {bacon_number}")
 
 
 if __name__ == "__main__":
--- setup.py
+++
@@ -1,9 +0,0 @@
-from setuptools import setup, find_packages
-
-setup(
-    name="Bacon Number",
-    description="Find Bacon number for each person.",
-    author="Or Levi",
-    author_email="orlevi78@gmail.com",
-    packages=find_packages(),
-)
